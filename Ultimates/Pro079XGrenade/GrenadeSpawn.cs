04b038a428765d3f6bb4837f7ae1fee6220b8795
smohan67 <siddhu.mohan6@gmail.com>
2021-09-22 01:16:02 -0500

    add in logic to spawn grenade

--- Ultimates/Pro079XGrenade/GrenadeSpawn.cs
+++ Ultimates/Pro079XGrenade/GrenadeSpawn.cs
@@ -1,20 +1,28 @@
-﻿namespace Pro079XGrenade
+﻿using System.Linq;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using InventorySystem.Items.ThrowableProjectiles;
+
+namespace Pro079XGrenade
 {
     using System;
     using CommandSystem;
     using Pro079X.Interfaces;
+    using System.Collections.Generic;
     public class GrenadeSpawn : IUltimate079
     {
+        public string Command { get; } = Pro079XGrenade.Plugin.Singleton.Translations.Command;
+        public string[] Aliases { get; }
+        public string Description { get; } = Pro079XGrenade.Plugin.Singleton.Translations.Description;
+        public int Cooldown { get; }
+        public int Cost { get; }
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            Player ply = Player.Get((sender as CommandSender)?.SenderId);
+            List<Player> players = ply.CurrentRoom.Players.ToList();
+            new ExplosiveGrenade(ItemType.SCP018, ply).SpawnActive(players[0].Position, ply);
             response = "Not implemented yet";
             return true;
         }
-
-        public string Command { get; } = Pro079XGrenade.Plugin.Singleton.Translations.Command;
-        public string[] Aliases { get; }
-        public string Description { get; }
-        public int Cooldown { get; }
-        public int Cost { get; }
     }
 }
