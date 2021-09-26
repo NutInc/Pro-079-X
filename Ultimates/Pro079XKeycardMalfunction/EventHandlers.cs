01ff87071dc161bf3e311087502ee9fca734f0ba
smohan67 <siddhu.mohan6@gmail.com>
2021-09-26 18:32:03 -0500

    fix build and add another ultimate

---
+++ Ultimates/Pro079XKeycardMalfunction/EventHandlers.cs
@@ -0,0 +1,19 @@
+﻿namespace Pro079XKeycardMalfunction
+{
+    using Exiled.Events.EventArgs;
+    using Interactables.Interobjects.DoorUtils;
+    
+    public static class EventHandlers
+    {
+        public static bool KeycardsBroken = false;
+
+        public static void OnInteractDoor(InteractingDoorEventArgs ev)
+        {
+            if (!KeycardsBroken) return;
+            if (!ev.IsAllowed) return;
+            if (ev.Door.RequiredPermissions.RequiredPermissions == KeycardPermissions.None) return;
+            ev.IsAllowed = false;
+            ev.Player.ShowHint( "Keycard Malfunction! Please wait for them to be fixed.");
+        }
+    }
+}
---
+++ Ultimates/Pro079XKeycardMalfunction/KeycardMalfunctionCommand.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Pro079XKeycardMalfunction
+{
+    using Pro079X.Interfaces;
+    using CommandSystem;
+    using System;
+    using Exiled.API.Features;
+    using MEC;
+    public class KeycardMalfunctionCommand : IUltimate079
+    {
+        public string Command { get; } = Pro079XKeycardMalfunction.Singleton.Translation.Command;
+        
+        public string Description { get; } = Pro079XKeycardMalfunction.Singleton.Translation.Description;
+        public int Cooldown { get; } = Pro079XKeycardMalfunction.Singleton.Config.Cooldown;
+        public int Cost { get; } = Pro079XKeycardMalfunction.Singleton.Config.Cost;
+        
+        public string[] Aliases { get; }
+        
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (EventHandlers.KeycardsBroken)
+            {
+                response = "Keycard Malfunction already occuring!";
+                return false;
+            }
+
+            EventHandlers.KeycardsBroken = true;
+            Cassie.Message(Pro079XKeycardMalfunction.Singleton
+                .Translation.CassieStartMessage);
+            Timing.CallDelayed(Pro079XKeycardMalfunction.Singleton.Config.Duration,()=>
+            {
+                EventHandlers.KeycardsBroken = false;
+                Cassie.Message(Pro079XKeycardMalfunction.Singleton
+                    .Translation.CassieEndMessage);
+            });
+
+            response = "We do some trolling";
+            return true;
+        }
+    }
+}
