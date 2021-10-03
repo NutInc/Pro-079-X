476909ac946010f2316fb523f36ecb26d4411a60
smohan67 <siddhu.mohan6@gmail.com>
2021-10-03 04:56:34 -0500

    plz work

--- Ultimates/Pro079XBlackout/BlackoutCommand.cs
+++ Ultimates/Pro079XBlackout/BlackoutCommand.cs
@@ -9,11 +9,11 @@
 
     public class BlackoutCommand : IUltimate079
     {
-        public string Command { get; } = Pro079XBlackout.Singleton.Translation.Command;
+        public string Command { get; } = Plugin.Singleton.Translation.Command;
         public string[] Aliases { get; } = Array.Empty<string>();
-        public string Description { get; } = Pro079XBlackout.Singleton?.Translation.Description;
-        public int Cooldown { get; } = Pro079XBlackout.Singleton.Config.Cooldown;
-        public int Cost { get; } = Pro079XBlackout.Singleton.Config.Cost;
+        public string Description { get; } = Plugin.Singleton?.Translation.Description;
+        public int Cooldown { get; } = Plugin.Singleton.Config.Cooldown;
+        public int Cost { get; } = Plugin.Singleton.Config.Cost;
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             Log.Debug("Execute() invoked for blackout!");
@@ -25,9 +25,9 @@
         private static IEnumerator<float> RunUltimate()
         {
             Log.Debug("Running ultimate coroutine for Blackout!");
-            Cassie.Message(Pro079XBlackout.Singleton.Config.BlackoutCassieMessage);
+            Cassie.Message(Plugin.Singleton.Config.BlackoutCassieMessage);
             yield return Timing.WaitForSeconds(12.1f);
-            Map.TurnOffAllLights(Pro079XBlackout.Singleton.Config.BlackoutDuration);
+            Map.TurnOffAllLights(Plugin.Singleton.Config.BlackoutDuration);
         }
     }
 }
