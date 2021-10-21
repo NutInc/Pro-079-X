9218ab4a23ac50dd3825d4d55e6c396a126e4323
smohan67 <siddhu.mohan6@gmail.com>
2021-10-21 10:32:30 -0500

    add chaos command

--- Modules/Pro079XChaos/Pro079XChaos.cs
+++ Modules/Pro079XChaos/Pro079XChaos.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using Exiled.API.Features;
 using Pro079X.Logic;
-using Pro079XGas.Configs;
+using Pro079XChaos.Configs;
 
-namespace Pro079XGas
+namespace Pro079XChaos
 {
-    public class Pro079XGas : Plugin<Config, Translations>
+    public class Pro079XChaos : Plugin<Config, Translations>
     {
         public override string Author => "NutInc";
         public override Version Version => new Version(4, 0, 0);
         public override string Name { get; } = "Pro079XGas";
-        internal static Pro079XGas Singleton;
+        internal static Pro079XChaos Singleton;
         internal Translations Translations;
 
         public override void OnEnabled()
@@ -18,14 +18,7 @@ namespace Pro079XGas
             Singleton = this;
             Translations = new Translations();
             base.OnEnabled();
-            Manager.RegisterCommand(new GasCommand());
-        }
-        
-        public override void OnDisabled()
-        {
-            base.OnDisabled();
-            Singleton = null;
-            Translations = null;
+            Manager.RegisterCommand(new ChaosCommand());
         }
     }
 }
